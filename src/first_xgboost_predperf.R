295c75049e63c16e18830fb275db7632cc40e3df
annvirkk <avirkkala@whrc-linux.cs196acloud.internal>
2021-02-24 04:20:06 -0500

    edits to modeling scripts

---
+++ src/first_xgboost_predperf.R
@@ -0,0 +1,332 @@
+
+
+
+# Packages
+library("caret")
+library("vip")
+library("pdp")
+library("ggplot2")
+library("viridis")
+
+### Data
+setwd("/mnt/data1/boreal/avirkkala/repos/flux_upscaling_data/src/")
+d <- read.csv("../results/final/modeldata_avg.csv")
+
+
+### Calculate the factor variables 
+### Factor conversions
+# note that randomForest and svmRadial can handle variables coded as "as.factor"
+# but xgboost cannot: https://github.com/dmlc/xgboost/issues/95
+# so we will need to do one-hot encoding
+
+# Thermokarst
+d <- as.data.frame(d)
+d$Thermokarst <- factor(d$Thermokarst)
+onehot <- model.matrix(~0+d[, 'Thermokarst'])
+attr(onehot, "dimnames")[[2]] <- paste("Thermokarst", levels(d$Thermokarst), sep="_")
+d <- cbind(d, onehot)
+
+# land cover
+# first change NA in ESA CCI to something else
+d$ESACCI_cavm_general_ESAwaterfix_broadevfix_ESACCI_CAVM_merged <- ifelse(is.na(d$ESACCI_cavm_general_ESAwaterfix_broadevfix_ESACCI_CAVM_merged), "0000", d$ESACCI_cavm_general_ESAwaterfix_broadevfix_ESACCI_CAVM_merged)
+d$ESACCI_cavm_general_ESAwaterfix_broadevfix_ESACCI_CAVM_merged <- factor(d$ESACCI_cavm_general_ESAwaterfix_broadevfix_ESACCI_CAVM_merged)
+onehot <- model.matrix(~0+d[, 'ESACCI_cavm_general_ESAwaterfix_broadevfix_ESACCI_CAVM_merged'])
+attr(onehot, "dimnames")[[2]] <- paste("ESACCI_cavm_general_ESAwaterfix_broadevfix_ESACCI_CAVM_merged", levels(d$ESACCI_cavm_general_ESAwaterfix_broadevfix_ESACCI_CAVM_merged), sep="_")
+d <- cbind(d, onehot)
+
+# Fire burn classes
+d$Number_of_days_since_fire_classes_MCD64A1_sites_cleaned <- factor(d$Number_of_days_since_fire_classes_MCD64A1_sites_cleaned)
+onehot <- model.matrix(~0+d[, 'Number_of_days_since_fire_classes_MCD64A1_sites_cleaned'])
+attr(onehot, "dimnames")[[2]] <- paste("Number_of_days_since_fire_classes_MCD64A1_sites_cleaned", levels(d$Number_of_days_since_fire_classes_MCD64A1_sites_cleaned), sep="_")
+d <- cbind(d, onehot)
+
+
+
+### Response variables
+resp_vars <- c("NEE_gC_m2", "GPP_gC_m2", "Reco_gC_m2") 
+
+### Models
+models <- c("xgboost")
+
+
+# For figures
+
+theme_pub <- theme_bw() + theme(panel.border=element_rect(size=1, colour="black"),
+                                axis.text=element_text(size=14, face="bold"),
+                                plot.title = element_text(size = 14, face = "bold"),
+                                axis.title=element_text(size=14, face="bold"), 
+                                plot.subtitle=element_text(size=14, face="bold", color="black"), 
+                                strip.text.x = element_text(size = 14, face="bold"),
+                                legend.text=element_text(size=14, face="bold"), legend.title=element_text(size=14))
+
+
+
+for (i in resp_vars) {
+  
+  # i <- "NEE_gC_m2"
+  
+  # empty varimp data frame
+  all_varImp <- NA
+  
+  
+  # Loop through the models 
+  
+  for (m in models) {
+    
+    # m <- "xgboost"
+
+    # Load model files
+    mod <- readRDS(paste0("/mnt/data1/boreal/avirkkala/repos/abcflux_modeling/results/", paste(i, "1km", m, "test", sep="_"), ".rds"))
+    # m <- readRDS(paste0("/mnt/data1/boreal/avirkkala/repos/abcflux_modeling/results/", paste(i, "1km_xgboost_test", sep="_"), ".rds"))  # remove _test when we have final
+    # rfFit <- readRDS(paste0("../results/", paste(i, "rf", sep="_"), ".rds"))
+    # svmFit <- readRDS(paste0("../results/", paste(i, "svm", sep="_"), ".rds"))
+    
+    
+    # Print the best variables
+    print("Best variables are:")
+    mod$bestSubset
+    mod$optVariables
+    Selected_vars_1km <- mod$optVariables
+
+    ### Predictive performance plots ###
+    
+    
+    # Define x and y lab titles for the plot
+    if (i=="GPP_gC_m2") {
+      ylab = expression(paste("Observed GPP g C m"^{-2}, month^{-1}))
+      xlab = expression(paste("Predicted GPP g C m"^{-2}, month^{-1}))
+      
+    }
+    
+    if (i=="NEE_gC_m2") {
+      ylab = expression(paste("Observed NEE g C m"^{-2}, month^{-1}))
+      xlab = expression(paste("Predicted NEE g C m"^{-2}, month^{-1}))
+      
+    }
+    
+    if (i=="Reco_gC_m2") {
+      ylab = expression(paste("Observed ER g C m"^{-2}, month^{-1}))
+      xlab = expression(paste("Predicted ER g C m"^{-2}, month^{-1}))
+      
+    }
+    
+    
+    
+    
+    # First plot scatterplots for each variable based on individual models
+    # Max and min of several columns 
+    scale_max <- max(c(mod$pred$obs, mod$pred$pred))
+    scale_min <- min(c(mod$pred$obs, mod$pred$pred))
+    
+    # scale_max <- max(c(gbmFit$pred$obs, gbmFit$pred$pred, rfFit$pred$obs, rfFit$pred$pred, svmFit$pred$obs, svmFit$pred$pred))
+    # scale_min <- min(c(gbmFit$pred$obs, gbmFit$pred$pred, rfFit$pred$obs, rfFit$pred$pred, svmFit$pred$obs, svmFit$pred$pred))
+    
+    
+    # Merge observations and predictions to one data frame, and merge it with cavm
+    # First need to sort the tables in the same order as in d
+    figdata <- data.frame(cbind(mod$pred$obs, mod$pred$pred))
+    names(figdata) <- c("obs", m)
+    
+    r <- merge(figdata, d, by.x="obs", by.y=i) # this is not ideal - there are duplicate observations that will mix this up
+
+    
+    # check whether Rsquared and correlation-derived estimate match
+    #cor(r$obs, r[, m])^2 # yep, they are the same!
+    #rmse(r$obs, r[, m])
+    #mean(abs(r$obs -r[, m]))
+    
+    # Merge pred perf
+    # not sure how to access the results from the best model! This is a shortcut: mod$results[which.min(mod$results[, "RMSE"]), ]
+    r_stats <- data.frame(cbind(model=c(m), RMSE=c(mod$results[which.min(mod$results[, "RMSE"]), ]$RMSE),
+                                Rsquared=(mod$results[which.min(mod$results[, "RMSE"]), ]$Rsquared),
+                                MAE=c(mod$results[which.min(mod$results[, "RMSE"]), ]$MAE)))
+    r_stats$RMSE <- as.character(r_stats$RMSE) %>% as.numeric()
+    r_stats$Rsquared <- as.character(r_stats$Rsquared) %>% as.numeric()
+    r_stats$MAE <- as.character(r_stats$MAE) %>% as.numeric()
+    
+    
+    ### Plot XGBOOST based on categorical information that we have
+    
+    # Colored by biome
+    p1 <- ggplot(r, aes(x=r[,m], y=obs, colour=factor(Biome))) +
+      geom_point(shape = 16,  size=3) + geom_abline(slope = 1) + 
+      
+      annotate(label = sprintf("MAE = %.1f   \nRsquared = %.2f   \nRMSE = %.1f   \n ", 
+                               r_stats %>% filter(model==m) %>% dplyr::select(MAE) %>% as.character() %>% as.numeric(), 
+                               r_stats %>% filter(model==m) %>% dplyr::select(Rsquared) %>% as.numeric(),
+                               r_stats %>% filter(model==m) %>% dplyr::select(RMSE) %>% as.numeric()), geom = "text", x = Inf, y = -Inf, size = 4, hjust = 1, vjust = 0) +
+      
+      theme_pub  + theme(legend.title=element_blank()) + scale_colour_viridis(discrete=TRUE) + xlab(m)
+    
+    # Print out
+    setwd("/mnt/data1/boreal/avirkkala/repos/abcflux_modeling/results/figures/")
+    p1
+    dev.copy(png, paste0(i, "_xgboost_predperf_biome.png"), width=500, height=400)
+    dev.off()
+    
+    
+    # Colored by veg type
+    p2 <- ggplot(r, aes(x=r[,m], y=obs, colour=factor(Veg_type2))) +
+      geom_point(shape = 16,  size=3) + geom_abline(slope = 1) + 
+      
+      annotate(label = sprintf("MAE = %.1f \nRsquared = %.2f \nRMSE = %.1f \n ", 
+                               r_stats %>% filter(model==m) %>% dplyr::select(MAE) %>% as.character() %>% as.numeric(), 
+                               r_stats %>% filter(model==m) %>% dplyr::select(Rsquared) %>% as.numeric(),
+                               r_stats %>% filter(model==m) %>% dplyr::select(RMSE) %>% as.numeric()), geom = "text", x = Inf, y = -Inf, size = 4, hjust = 1, vjust = 0) +
+      
+      theme_pub  + theme(legend.title=element_blank()) + scale_colour_viridis(discrete=TRUE) + xlab(m)
+    
+    p2
+    dev.copy(png, paste0(i, "_xgboost_predperf_vegtype.png"), width=500, height=400)
+    dev.off()
+    
+    
+    # Colored by disturbance
+    r$Disturbance <- ifelse(is.na(r$Disturbance), "NA", r$Disturbance)
+    p3 <- ggplot(r, aes(x=r[,m], y=obs, colour=factor(Disturbance))) +
+      geom_point(shape = 16,  size=3) + geom_abline(slope = 1) + 
+      
+      annotate(label = sprintf("MAE = %.1f \nRsquared = %.2f \nRMSE = %.1f \n ", 
+                               r_stats %>% filter(model==m) %>% dplyr::select(MAE) %>% as.character() %>% as.numeric(), 
+                               r_stats %>% filter(model==m) %>% dplyr::select(Rsquared) %>% as.numeric(),
+                               r_stats %>% filter(model==m) %>% dplyr::select(RMSE) %>% as.numeric()), geom = "text", x = Inf, y = -Inf, size = 4, hjust = 1, vjust = 0) +
+      
+      theme_pub  + theme(legend.title=element_blank())  + scale_colour_viridis(discrete=TRUE) + xlab(m)
+    
+    p3
+    dev.copy(png, paste0(i, "_xgboost_predperf_disturbance.png"), width=500, height=400)
+    dev.off()
+    
+    
+    # Colored by Study ID Clean
+    p4 <- ggplot(r, aes(x=r[,m], y=obs, colour=factor(Study_ID_Clean))) +
+      geom_point(shape = 16,  size=3) + geom_abline(slope = 1) + 
+      
+      annotate(label = sprintf("MAE = %.1f \nRsquared = %.2f \nRMSE = %.1f \n ", 
+                               r_stats %>% filter(model==m) %>% dplyr::select(MAE) %>% as.character() %>% as.numeric(), 
+                               r_stats %>% filter(model==m) %>% dplyr::select(Rsquared) %>% as.numeric(),
+                               r_stats %>% filter(model==m) %>% dplyr::select(RMSE) %>% as.numeric()), geom = "text", x = Inf, y = -Inf, size = 4, hjust = 1, vjust = 0) +
+      
+      theme_pub  + theme(legend.title=element_blank())  + scale_colour_viridis(discrete=TRUE) + xlab(m)
+    
+    p4
+    dev.copy(png, paste0(i, "_xgboost_predperf_studyID.png"), width=500, height=400)
+    dev.off()
+    
+    
+    # Colored by flux method
+    p5 <- ggplot(r, aes(x=r[,m], y=obs, colour=factor(Flux_method))) +
+      geom_point(shape = 16,  size=3) + geom_abline(slope = 1) + 
+      
+      annotate(label = sprintf("MAE = %.1f \nRsquared = %.2f \nRMSE = %.1f \n ", 
+                               r_stats %>% filter(model==m) %>% dplyr::select(MAE) %>% as.character() %>% as.numeric(), 
+                               r_stats %>% filter(model==m) %>% dplyr::select(Rsquared) %>% as.numeric(),
+                               r_stats %>% filter(model==m) %>% dplyr::select(RMSE) %>% as.numeric()), geom = "text", x = Inf, y = -Inf, size = 4, hjust = 1, vjust = 0) +
+      
+      theme_pub  + theme(legend.title=element_blank())  + scale_colour_viridis(discrete=TRUE) + xlab(m)
+    
+    p5
+    dev.copy(png, paste0(i, "_xgboost_predperf_fluxmethod.png"), width=500, height=400)
+    dev.off()
+    
+    
+    # Colored by flux method detail
+    p6 <- ggplot(r, aes(x=r[,m], y=obs, colour=factor(Flux_method_detail))) +
+      geom_point(shape = 16,  size=3) + geom_abline(slope = 1) + 
+      
+      annotate(label = sprintf("MAE = %.1f \nRsquared = %.2f \nRMSE = %.1f \n ", 
+                               r_stats %>% filter(model==m) %>% dplyr::select(MAE) %>% as.character() %>% as.numeric(), 
+                               r_stats %>% filter(model==m) %>% dplyr::select(Rsquared) %>% as.numeric(),
+                               r_stats %>% filter(model==m) %>% dplyr::select(RMSE) %>% as.numeric()), geom = "text", x = Inf, y = -Inf, size = 4, hjust = 1, vjust = 0) +
+      
+      theme_pub  + theme(legend.title=element_blank()) + scale_colour_viridis(discrete=TRUE) + xlab(m)
+    
+    
+    p6
+    dev.copy(png, paste0(i, "_xgboost_predperf_fluxmethoddetail.png"), width=500, height=400)
+    dev.off()
+    
+    
+    
+    # Colored by Measurement frequency
+    p7 <- ggplot(r, aes(x=r[,m], y=obs, colour=factor(Measurement_frequency))) +
+      geom_point(shape = 16,  size=3) + geom_abline(slope = 1) + 
+      
+      annotate(label = sprintf("MAE = %.1f \nRsquared = %.2f \nRMSE = %.1f \n ", 
+                               r_stats %>% filter(model==m) %>% dplyr::select(MAE) %>% as.character() %>% as.numeric(), 
+                               r_stats %>% filter(model==m) %>% dplyr::select(Rsquared) %>% as.numeric(),
+                               r_stats %>% filter(model==m) %>% dplyr::select(RMSE) %>% as.numeric()), geom = "text", x = Inf, y = -Inf, size = 4, hjust = 1, vjust = 0) +
+      
+      theme_pub  + theme(legend.title=element_blank())  + scale_colour_viridis(discrete=TRUE) + xlab(m)
+    
+    p7
+    dev.copy(png, paste0(i, "_xgboost_predperf_measfreq.png"), width=500, height=400)
+    dev.off()
+    
+  
+    
+    
+    ### Variable importance calculation
+    varImp <- vip(mod, method="permute", train=r[, Selected_vars_1km], target = r$obs, metric = "rsquared",
+                      pred_wrapper = predict, nsim=100) # only 10 most important ones selected
+    
+    
+    
+
+  } # model loop done
+  
+  ### Variable importance plots
+  # Extract data and combine into one data frame
+  all_varImp <- rbind(all_varImp, cbind(varImp$data, "Model"=c(m)))
+
+  all_varImp$Model <- ifelse(all_varImp$Model=="xgboost", "XGBOOST", all_varImp$Model)
+  all_varImp$Model <- ifelse(all_varImp$Model=="rf", "RF", all_varImp$Model)
+  all_varImp$Model <- ifelse(all_varImp$Model=="svm", "SVM", all_varImp$Model)
+  all_varImp$Model <- factor(all_varImp$Model)
+  #all_varImp$Model <- factor(all_varImp$Model, levels=c("XGBOOST", "RF", "SVM"))
+  
+  all_varImp <- all_varImp[-1, ]
+  
+  # Print out
+  print(ggplot(all_varImp) + geom_bar(aes(x=Variable, y=Importance, fill=Model), stat="identity", position="dodge")  + 
+          scale_fill_manual(values = c(viridis(3)[1],viridis(3)[2], viridis(3)[3]), guide = guide_legend(reverse=TRUE))  + 
+          coord_flip() +
+          theme_pub) #+ ggtitle(title2)
+  
+  dev.copy(png, paste0( i, "_vip.png"), width=650, height=400)
+  dev.off()
+  
+  
+  
+  
+  
+}
+
+
+
+
+
+
+### Partial dependence plots need to be done in a separate loop
+
+
+# Partial dependence plots
+
+# First baseline var
+pd1 <- partial(gbmFit, pred.var = Baseline_vars[1], train=data1)  # don't set plot = TRUE
+pd2 <- partial(rfFit, pred.var = Baseline_vars[1], train=data1)
+pd3 <- partial(svmFit, pred.var = Baseline_vars[1], train=data1)
+
+
+# ggplot2
+pd1$Model <- "GBM"  # add new column
+pd2$Model <- "RF"
+pd3$Model <- "SVM"
+
+pd.all1 <- rbind(pd1, pd2, pd3)  # bind rows
+
+pdp_plot1 <- ggplot(pd.all1, aes(x=pd.all1[, 1], y=pd.all1[, 2], color = Model)) +
+  geom_line(size=1) + theme_pub  +labs(y="yhat", x=Baseline_vars[1]) + 
+  scale_color_manual(values = c(viridis(3)[3],viridis(3)[2], viridis(3)[1]), guide = guide_legend(reverse=TRUE)) +
+  theme(legend.position = "none") # + ggtitle(title)
+
---
+++ src/potential_data_splits.R
@@ -0,0 +1,270 @@
+
+
+
+
+### Different model data extents ###
+
+
+### Split to data from 2001-2019 and 1981-2017
+
+
+### Split GS and NGS
+
+
+### Same as original, but winter NEE -> Reco and GPP==0
+d2 <- d
+d2$Reco_gC_m2 <- ifelse(is.na(d2$Reco_gC_m2) & d2$Season=="winter", d2$NEE_gC_m2, d2$Reco_gC_m2)
+d2$GPP_gC_m2 <- ifelse(is.na(d2$GPP_gC_m2) & d2$Season=="winter", 0, d2$Reco_gC_m2)
+
+### Winter NEE -> Reco only in the tundra?
+d3 <- d
+d3$Reco_gC_m2 <- ifelse(is.na(d3$Reco_gC_m2) & d3$Season=="winter" & d3$Biome=="Tundra", d3$NEE_gC_m2, d3$Reco_gC_m2)
+d3$GPP_gC_m2 <- ifelse(is.na(d3$GPP_gC_m2) & d3$Season=="winter" & d3$Biome=="Tundra", 0, d3$Reco_gC_m2)
+
+### Data filtering based on QAQC and Gapfill
+
+hist(d$Tower_QA.QC.NEE.flag)
+hist(d$Gap_perc)
+
+d4 <- subset(d, Tower_QA.QC.NEE.flag>0.2 & Gap_perc<80 | (is.na(Tower_QA.QC.NEE.flag) & is.na(Gap_perc)))
+
+# d4$NEE_ALT_regr <- d4$Reco_gC_m2+d4$GPP_gC_m2
+# plot(d4$NEE_gC_m2, d4$NEE_ALT_regr)
+
+
+
+### Keep rows that have NEE, GPP, and Reco
+
+d5 <- subset(d, !(is.na(d$Reco_gC_m2)) & !(is.na(d$GPP_gC_m2)) & !(is.na(d$NEE_gC_m2)))
+
+
+
+### Remove observations where NEE and GP-Reco don't match
+
+d6 <- d
+d6$NEE_ALT_regr <- d6$Reco_gC_m2+d6$GPP_gC_m2
+plot(d6$NEE_ALT_regr, d6$NEE_gC_m2)
+m1 <- lm(NEE_gC_m2 ~ NEE_ALT_regr, d6 )
+residuals <- as.vector(residuals(m1))
+d6$resid <- NA
+length(d6$resid[!(is.na(d6$Reco_gC_m2)) & !(is.na(d6$GPP_gC_m2)) & !(is.na(d6$NEE_gC_m2))])
+d6$resid[!(is.na(d6$Reco_gC_m2)) & !(is.na(d6$GPP_gC_m2)) & !(is.na(d6$NEE_gC_m2))] <- residuals
+hist(d6$resid)
+
+# Data points with a residual outside the range of ±3 times the inter-quartile range were removed. # IQR(x) = quantile(x, 3/4) - quantile(x, 1/4)
+# IQR(d6$resid, na.rm=TRUE)
+# more details here: https://stats.stackexchange.com/questions/175999/determine-outliers-using-iqr-or-standard-deviation
+# Lower fence = Q1 - (IQR * multiplier)
+# Upper fence = Q3 + (IQR * multiplier)
+IQR(d6$resid, na.rm=TRUE)
+quantile(d6$resid, 3/4, na.rm=TRUE)
+quantile(d6$resid, 1/4, na.rm=TRUE)
+
+d6$pot_remov <- ifelse(d6$resid<(quantile(d6$resid, 1/4, na.rm=TRUE)-(IQR(d6$resid, na.rm=TRUE)*3)) | is.na(d6$resid), "remove", "keep")
+d6$pot_remov <- ifelse(d6$resid>(quantile(d6$resid, 3/4, na.rm=TRUE)+(IQR(d6$resid, na.rm=TRUE)*3)) | is.na(d6$resid), "remove", d6$pot_remov)
+d6$pot_remov <- ifelse(is.na(d6$GPP_gC_m2), "keep", d6$pot_remov)
+
+d6 %>% group_by(pot_remov) %>% summarise(n=n()) # wow, removes 961 obs
+
+d6 <- subset(d6, pot_remov=="keep")
+
+
+
+### Remove chamber measurements conducted during daytime only
+
+d7 <- subset(d, Diurnal_coverage!="Day")
+
+
+###
+
+### Remove measurements conducted at the same time at the same site with both tower, chamber/diffusion ###
+# i.e. these are duplicates!
+
+
+### DO THIS AGAIN WITH THE FINAL FLUX TABLE!!! AND MAYBE CHANGE THE BUFFER TO 250 M or 200 M (i.e. 400-500 m)
+### AND CHECK that below should be d3, not d2
+
+# this was already ran - so just load the overlap file
+
+# # each site to a sf object with coordinates
+# sp <- st_as_sf(d2, coords=c("Longitude", "Latitude"))
+# plot(sp["NEE_gC_m2"])
+# sp
+# 
+# # add crs
+# sp <- st_set_crs(sp, "+proj=longlat +datum=WGS84") 
+# 
+# # transform to a projected (not geographic) system, pseudo-Mercator: https://epsg.io/3857
+# sp <- st_transform(sp, "+init=epsg:3857")
+# plot(sp["NEE_gC_m2"])
+# 
+# 
+# # create buffer around each site. buffer 500-2000-4000 m to each direction
+# sp_buffer <- st_buffer(sp, dist=500) # this changes each point to be a polygon!
+# plot(sp_buffer["Study_ID"])
+# 
+# 
+# # what about intersect?
+# overlaps_all <- data.frame(matrix(ncol =2, nrow = 0))
+# colnames(overlaps_all) <- c("Study_ID", "Overlapping_IDs")
+# 
+# characterfunction <- function(x){paste(unique(x),  collapse=',')}
+# # loop through sites
+# for (i in 1:nrow(sp)) {
+#   
+#   print(i)
+#   sp_one <- sp[i, ]
+#   # 
+#   sp_overlap <- st_intersects(sp_one, sp_buffer, sparse =  FALSE)
+#   
+#   overlaps <- as.vector(sp_overlap)
+#   
+#   overlapping_sites <- sp_buffer$Study_ID[overlaps==TRUE] %>% unique()
+#   overlapping_sites_unique <- characterfunction(overlapping_sites)
+#   
+#   overlaps_one <- cbind(data.frame(Study_ID=sp_one$Study_ID, Overlapping_IDs=overlapping_sites_unique))
+#   
+#   overlaps_all <- rbind(overlaps_all, overlaps_one)
+# }
+# 
+# 
+# overlaps_final <- unique(overlaps_all)
+# 
+# overlaps <- overlaps_final %>% group_by(Study_ID) %>% summarize(Overlapping_IDs=paste(unique(Overlapping_IDs),  collapse=','))
+# 
+# overlaps <- subset(overlaps, Study_ID!=Overlapping_IDs)
+
+
+### THESE ALSO NEED TO BE DONE AGAIN
+#overlaps_full <- read.csv("E:/UniversityComputer/ANNVIRKK/documents/WHRC/Flux_database_merging_final/filtering/overlaps.csv")
+overlaps <- read.csv("/mnt/data1/boreal/avirkkala/repos/flux_upscaling_data/data/overlaps_cleaned.csv")
+
+
+# Loop through the rows in overlaps file
+# extract the different study IDs and subset the data frame based on those
+
+library("stringr")
+# remove empty characters
+overlaps$Overlapping_IDs <- str_replace_all(string=overlaps$Overlapping_IDs, pattern=" ", repl="")
+
+for (i in 1:nrow(overlaps)) {
+  
+  print(i)
+  # i <- 1
+  ids <- overlaps$Overlapping_IDs[i]
+  print(ids)
+  
+  iddat <- sapply(strsplit(ids, ","), "[") %>% data.frame()
+  names(iddat) <- "sites"
+  
+  iddata <- subset(d3, d3$Study_ID %in% iddat$sites)
+  
+  # is there temporal overlap?
+  print(duplicated(iddata$Start_date))
+  
+  if(any(duplicated(iddata$Start_date)) ==TRUE) {
+    print(unique(iddata$Veg_type2))
+  }
+  
+  # what will the time series look like?
+  if (any(!is.na(iddata$NEE_gC_m2))) {
+    
+    #print(ggplot(iddata) + geom_point(aes(x=as.Date(Start_date), NEE_gC_m2, col=Study_ID))) #plot(as.Date(iddata$Start_date), iddata$NEE_gC_m2)
+    
+    
+  }
+  
+  # THis is for Minions
+  if (any(is.na(iddata$NEE_gC_m2))) {
+    
+    #print(ggplot(iddata) + geom_point(aes(x=as.Date(Start_date), Rsoil_gC_m2, col=Study_ID))) #plot(as.Date(iddata$Start_date), iddata$NEE_gC_m2)
+    
+    
+  }
+  
+  
+}
+
+# not many measurements from the same months, time series look OK.
+# I think we could just take the mean of the average values, paste the e
+
+
+
+######################## REMEMBER TO CHECK Diurnal_coverage SOMEWHERE HERE !!!!!!!!!!!!!!!!!!!!!! ##############################
+# Now we are merging measurements that cover the whole day and don't cover the whole day!! -> not ideal
+# Select the one that covers the whole day first, and if both are similar then average
+# BUT NOTE - THIS ALSO INFLUENCES many other columns, e.g. Gap fill etc??
+
+
+
+
+
+for (i in 1:nrow(overlaps)) {
+  
+  print(i)
+  # i <- 1
+  ids <- overlaps$Overlapping_IDs[i]
+  print(ids)
+  
+  # list the sites
+  iddat <- sapply(strsplit(ids, ","), "[") %>% data.frame()
+  names(iddat) <- "sites"
+  
+  # subset the dataframe to those sites
+  iddata <- subset(d3, d3$Study_ID %in% iddat$sites)
+  
+  
+  if(any(duplicated(iddata$Start_date)) ==TRUE) {
+    
+    print(duplicated(iddata$Start_date))
+    
+    # calculate the mean
+    iddata2 <- iddata %>% group_by(Start_date) %>% select_if(is.character) %>%
+      summarise_all(characterfunction) 
+    
+    iddata3 <- iddata %>% group_by(Start_date)%>% select_if(is.numeric) %>%
+      summarise_all(mean, na.rm = TRUE)
+    
+    # some columns have NA -> is not captured by the above commants
+    iddata4 <- iddata %>% group_by(Start_date)%>% select_if(is.logical) %>%
+      summarise_all(mean, na.rm = TRUE)
+    
+    # remove Start_date from one 
+    iddata3 <- subset(iddata3, select=-c(Start_date))
+    iddata4 <- subset(iddata4, select=-c(Start_date))
+    
+    
+    # merge
+    iddatafinal <- cbind(iddata2, iddata3, iddata4)
+    
+    which(!(names(iddata) %in% names(iddatafinal)))
+    names(iddata)[which(!(names(iddata) %in% names(iddatafinal)))]
+    
+    
+    # return the column order
+    iddatafinal<-iddatafinal[names(iddata)]
+    
+    
+    # prints
+    print(paste("size of the original iddata table:", nrow(iddata)))
+    print(paste("size of the  iddatafinal table after averaging:", nrow(iddatafinal)))
+    
+    # replace
+    print(paste("size of the  flux table before merging:", nrow(d3)))
+    d4 <- subset(d3, !(d3$Study_ID %in% iddat$sites))
+    d3 <- rbind(d4, iddatafinal)
+    
+    print(paste("size of the  flux table after merging:", nrow(d3)))
+    
+  }
+  
+}
+
+
+# tämä sekottaa vähän dataa, vaikeampiselkoista - esim. jos torni ja kammio sekoittuu niin näin sekoittuu myös esim. gapfill-tieto
+
+
+
+
+### And different combinations of these
+
